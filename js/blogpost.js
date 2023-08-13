6c99177a0d3b557a87fe50e62a52fcdcc04011dc
Crash27coding <99522377+mohit2202solanki@users.noreply.github.com>
2023-08-13 14:39:08 +0530

    blogpost.js

---
+++ js/blogpost.js
@@ -0,0 +1,10 @@
+//let data = document.getElementsByClassName('h4.title').innerHtml;
+
+let data= document.getElementsByClassName("h4.title");
+data[0].addEventListener('click', myfun);
+
+function myfun()
+{
+    document.getElementsById('msg')[0].innerHTML= "helloo";
+  console.log("xcnksjecnj");
+}
